c754eb6a55340b2c114445cac4f567feb74aad1a
gernot <gernot@3d4b920f-9040-0410-a9fc-4385bbd6327c>
2007-12-05 21:13:28 +0000

    Changed Copyright Notice

--- RMPreferencesController.h
+++ RMPreferencesController.h
@@ -3,7 +3,7 @@
 //  Readomatic
 //
 //  Created by Gernot Poetsch 04.05.07.
-//  Copyright 2007 Gernot Poetsch. All rights reserved.
+//  Copyright 2007 Gernot Poetsch. Released under GLPv3. Have Fun!
 //
 
 #import <Cocoa/Cocoa.h>
